61226e451755d9b7c0e1e592982b2dd489e000cc
Travis Keep <keep94@gmail.com>
2019-07-13 05:18:04 -0700

    Add Harshads.

---
+++ harshads.go
@@ -0,0 +1,44 @@
+package gomath
+
+import (
+  "context"
+)
+
+// Harshads generates the harshad numbers in order that are greater than or
+// equal to start.
+func Harshads(ctx context.Context, start int64) <-chan int64 {
+  if start < 1 {
+    start = 1
+  }
+  sum := sumDigits(start)
+  result := make(chan int64)
+  go func() {
+    defer close(result)
+    for {
+      if start % sum == 0 {
+        select {
+          case <-ctx.Done():
+            return
+          case result <- start:
+        }
+      }
+      start++
+      sum++
+      temp := start
+      for temp % 10 == 0 {
+        temp /= 10
+        sum -= 9
+      }
+    }
+  }()
+  return result
+}
+
+func sumDigits(n int64) int64 {
+  result := int64(0)
+  for n > 0 {
+    result += n % 10
+    n /= 10
+  }
+  return result
+}
---
+++ harshads_test.go
@@ -0,0 +1,50 @@
+package gomath_test
+
+import (
+  "context"
+  "testing"
+
+  "github.com/keep94/gomath"
+)
+
+func TestHarshads(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  harshads := gomath.Harshads(ctx, 90)
+  checkInfInt64Chan(
+      t,
+      harshads,
+      90, 100, 102, 108, 110, 111, 112, 114, 117, 120)
+}
+
+func TestNthHarshad(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  indexer := gomath.NewIntIndexer(gomath.Harshads(ctx, 0))
+  n, _ := indexer.Nth(33)
+  assertEqual(t, int64(100), n)
+  n, _ = indexer.Nth(100)
+  assertEqual(t, int64(372), n)
+}
+
+func BenchmarkHarshads(b *testing.B) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  harshads := gomath.Harshads(ctx, 1)
+  i := 0
+  b.ResetTimer()
+  for range harshads {
+    i++
+    if i == b.N {
+      break
+    }
+  }
+}
+    
+func TestHarshadsContext(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  harshads := gomath.Harshads(ctx, 1)
+  cancel()
+  for _ = range harshads {
+  }
+}
