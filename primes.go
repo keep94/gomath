21316cba013b2c5d5e78c7325f30f023c720ec1d
Travis Keep <keep94@gmail.com>
2019-07-17 05:32:28 -0700

    Add Primes function.

---
+++ primes.go
@@ -0,0 +1,67 @@
+package gomath
+
+import (
+  "context"
+)
+
+// Primes generates the prime numbers in order that are greater than or
+// equal to start.
+func Primes(ctx context.Context, start int64) <-chan int64 {
+  result := make(chan int64)
+  go func() {
+    defer close(result)
+    if start <= 2 {
+      select {
+        case <-ctx.Done():
+          return
+        case result <- 2:
+      }
+      start = 3
+    } else {
+      start = start / 2 * 2 + 1  // start on odd
+    }
+    sieveSize := int64(1000)
+    for {
+      sieve := initSieve(int(sieveSize))
+      factor := int64(3)
+      for factor*factor < start + sieveSize*2 {
+        var mult int64
+        if factor*factor >= start {
+          mult = factor * factor
+        } else {
+          // smallest odd multiple of factor that is >= start
+          mult = (start + factor - 1) / (2*factor) * (2*factor) + factor
+        }
+        for mult < start + sieveSize*2 {
+          sieve[int((mult - start) / 2)] = false
+          mult += (2 * factor)
+        }
+        factor += 2
+      }
+      idx := int64(0)
+      for idx < sieveSize {
+        if sieve[int(idx)] {
+          select {
+            case <-ctx.Done():
+              return
+            case result <- start + 2*idx:
+          }
+        }
+        idx++
+      }
+      start += sieveSize*2
+      if sieveSize < 1000000 {
+        sieveSize *= 2
+      }
+    }
+  }()
+  return result
+}
+
+func initSieve(sieveSize int) []bool {
+  result := make([]bool, sieveSize)
+  for i := range result {
+    result[i] = true
+  }
+  return result
+}
---
+++ primes_test.go
@@ -0,0 +1,72 @@
+package gomath_test
+
+import (
+  "context"
+  "testing"
+
+  "github.com/keep94/gomath"
+)
+
+func TestPrimes(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  primes := gomath.Primes(ctx, 2)
+  checkInfInt64Chan(
+      t,
+      primes,
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+      67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137)
+}
+
+func TestPrimesBigStart(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  primes := gomath.Primes(ctx, 991)
+  checkInfInt64Chan(
+      t,
+      primes,
+      991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039)
+  ch := gomath.NewIntChan(gomath.Primes(ctx, 1000))
+  p1229 := ch.Nth(1229-168)
+  p1230 := ch.Nth(1230-168)
+  assertTrue(t, p1229 < 10000)
+  assertTrue(t, p1230 > 10000)
+}
+  
+
+func TestNthPrime(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  ch := gomath.NewIntChan(gomath.Primes(ctx, 0))
+  assertEqual(t, int64(997), ch.Nth(168))
+  p1229 := ch.Nth(1229)
+  p1230 := ch.Nth(1230)
+  assertTrue(t, p1229 < 10000)
+  assertTrue(t, p1230 > 10000)
+  p9592 := ch.Nth(9592)
+  p9593 := ch.Nth(9593)
+  assertTrue(t, p9592 < 100000)
+  assertTrue(t, p9593 > 100000)
+}
+
+func BenchmarkPrimes(b *testing.B) {
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  primes := gomath.Primes(ctx, 2)
+  i := 0
+  b.ResetTimer()
+  for range primes {
+    i++
+    if i == b.N {
+      break
+    }
+  }
+}
+    
+func TestPrimesContext(t *testing.T) {
+  ctx, cancel := context.WithCancel(context.Background())
+  primes := gomath.Primes(ctx, 2)
+  cancel()
+  for range primes {
+  }
+}
