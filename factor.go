7b85a319240e28e8e5276057b45a990227ccfdaa
Travis Keep <keep94@gmail.com>
2020-10-04 15:59:15 -0700

    Add Factors() method.

--- factor.go
+++ factor.go
@@ -1,12 +1,16 @@
 package gomath
 
+import (
+	"sort"
+)
+
 // PrimePower represents a single term of a prime power decomposition
 type PrimePower struct {
 	Prime int64
 	Power int
 }
 
-// Factor returns the prime power decomposition of n
+// Factor returns the prime power decomposition of n. Factor panics if n < 1.
 func Factor(n int64) []PrimePower {
 	if n < 1 {
 		panic("n can't be less than 1")
@@ -27,5 +31,33 @@ func Factor(n int64) []PrimePower {
 	if n > 1 {
 		result = append(result, PrimePower{Prime: n, Power: 1})
 	}
+	return result
+}
+
+// Factors returns all the positive integers that divide n in ascending order.
+// Factors panics if n < 1.
+func Factors(n int64) []int64 {
+	return ppdFactors(Factor(n))
+}
+
+func ppdFactorCount(ppd []PrimePower) int {
+	result := 1
+	for _, pp := range ppd {
+		result *= (pp.Power + 1)
+	}
+	return result
+}
+
+func ppdFactors(ppd []PrimePower) []int64 {
+	result := make([]int64, ppdFactorCount(ppd))
+	result[0] = 1
+	length := 1
+	for _, pp := range ppd {
+		for i := 0; i < length*pp.Power; i++ {
+			result[length+i] = result[i] * pp.Prime
+		}
+		length *= (pp.Power + 1)
+	}
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
 	return result
 }
--- factor_test.go
+++ factor_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/keep94/gomath"
+	asserts "github.com/stretchr/testify/assert"
 )
 
 func TestFactor(t *testing.T) {
@@ -34,4 +35,41 @@ func TestFactor(t *testing.T) {
 	assertPP(t, gomath.Factor(10013), 17, 1, 19, 1, 31, 1)
 	assertPP(t, gomath.Factor(10080), 2, 5, 3, 2, 5, 1, 7, 1)
 	assertPanic(t, func() { gomath.Factor(0) })
+}
+
+func TestFactors(t *testing.T) {
+	assert := asserts.New(t)
+	assert.Equal([]int64{1}, gomath.Factors(1))
+	assert.Equal([]int64{1, 2}, gomath.Factors(2))
+	assert.Equal([]int64{1, 3}, gomath.Factors(3))
+	assert.Equal([]int64{1, 2, 4}, gomath.Factors(4))
+	assert.Equal([]int64{1, 2, 3, 6}, gomath.Factors(6))
+	assert.Equal([]int64{1, 2, 3, 5, 6, 10, 15, 30}, gomath.Factors(30))
+	assert.Equal(
+		[]int64{1, 2, 3, 4, 6, 8, 9, 12, 18, 24, 36, 72},
+		gomath.Factors(72))
+	assert.Equal(
+		[]int64{
+			1,
+			2,
+			3,
+			4,
+			5,
+			6,
+			10,
+			12,
+			15,
+			20,
+			25,
+			30,
+			50,
+			60,
+			75,
+			100,
+			150,
+			300},
+		gomath.Factors(300))
+	assert.Panics(func() {
+		gomath.Factors(0)
+	})
 }
