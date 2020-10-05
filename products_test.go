db4a0615cc4f4161f50858dd242adfd035b2006e
Travis Keep <keep94@gmail.com>
2020-10-04 20:26:25 -0700

    Add tests for ProductsSlice().

--- products_test.go
+++ products_test.go
@@ -15,27 +15,34 @@ func TestProducts(t *testing.T) {
 		1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 30, 36)
 }
 
+func TestProductsSlice(t *testing.T) {
+	assert := asserts.New(t)
+	assert.Equal(
+		[]int64{1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 30, 36},
+		gomath.ProductsSlice(6, 2))
+}
+
 func TestZeroProducts(t *testing.T) {
-	products := gomath.Products(8, 0)
-	checkInt64Stream(t, products, 1)
+	assert := asserts.New(t)
+	assert.Equal([]int64{1}, gomath.ProductsSlice(8, 0))
 }
 
 func TestOneProduct(t *testing.T) {
-	products := gomath.Products(4, 1)
-	checkInt64Stream(t, products, 1, 2, 3, 4)
+	assert := asserts.New(t)
+	assert.Equal([]int64{1, 2, 3, 4}, gomath.ProductsSlice(4, 1))
 }
 
 func TestProductOfOne(t *testing.T) {
-	products := gomath.Products(1, 4)
-	checkInt64Stream(t, products, 1)
+	assert := asserts.New(t)
+	assert.Equal([]int64{1}, gomath.ProductsSlice(1, 4))
 }
 
-func TestProductsPanic(t *testing.T) {
+func TestProductsSlicePanic(t *testing.T) {
 	assert := asserts.New(t)
 	assert.Panics(func() {
-		gomath.Products(0, 4)
+		gomath.ProductsSlice(0, 4)
 	})
 	assert.Panics(func() {
-		gomath.Products(5, -1)
+		gomath.ProductsSlice(5, -1)
 	})
 }
